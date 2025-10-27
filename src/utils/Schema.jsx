66b427d92afeceffac9b41e1743d249099bd6b2e
sipan younan <sipannyounan@gmail.com>
2025-10-27 11:53:18 +0330

    add error messages

--- src/utils/Schema.jsx
+++ src/utils/Schema.jsx
@@ -1,6 +1,7 @@
 import { email, string, z } from "zod";
 const formSchema = z.object({
   name: string()
+    .nonempty("پر کردن این فیلد الزامی است")
     .regex(/^[\u0600-\u06FF\s]+$/, {
       message: "لطفا فقط از حروف فارسی استفاده کنید",
     })
@@ -11,13 +12,16 @@ const formSchema = z.object({
         }
       },
     }),
-  lastName: string().min(2, {
-    error: (iss) => {
-      if (iss.code === "too_small") {
-        return { message: "نام خانوادگی حداقل باید دو حرف داشته باشد" };
-      }
-    },
-  }),
+
+  lastName: string()
+    .nonempty("پر کردن این فیلد الزامی است")
+    .min(2, {
+      error: (iss) => {
+        if (iss.code === "too_small") {
+          return { message: "نام خانوادگی حداقل باید دو حرف داشته باشد" };
+        }
+      },
+    }),
   email: email("آدرس ایمیل را بصورت صحیح وارد نمایید"),
   types: z.enum(["پشتیبانی", "عمومی"], {
     error: "لطفا دسته بندی پیام خود را انتخاب کنید",
